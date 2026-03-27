27e75094d3c3aa7eb69f8218313bc37a5fdc0418
se2014-afk <sevashko2014@gmail.com>
2026-03-27 22:16:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express=require('express');
-const path=require('path');
-const app=express();
-const port=process.env.PORT || 3000;
-app.use((req, res, next)=>{res.set({
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+app.use((req, res, next) => {
+res.set({
 'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
 'Pragma': 'no-cache',
 'Expires': '0',
@@ -11,18 +12,24 @@ app.use((req, res, next)=>{res.set({
 });
 next();
 });
-app.get('/favicon.ico', (req, res)=>res.status(204).end());
-app.get('/', (req, res)=>{res.send(`<img src="/image.jpg?t=${Date.now()}" 
-style="object-fit:contain;margin:0;background:#000;overflow:hidden;width:100vw;height:100vh;cursor:pointer;">
+app.get('/favicon.ico', (req, res) => res.status(204).end());
+app.get('/', (req, res) => {
+res.send(`
+<img src="/image.jpg?t=${Date.now()}" style="object-fit:contain;margin:0;background:#000;overflow:hidden;width:100vw;height:100vh;cursor:pointer;">
 <script>
-document.body.addEventListener('click', function() {window.open('https://www.youtube.com','_blank');});
-if('serviceWorker' in navigator){navigator.serviceWorker.getRegistrations().then(regs=>{for(let reg of regs) reg.unregister();
+document.body.addEventListener('click', function() {
+window.open('https://www.youtube.com', '_blank');
+});
+if ('serviceWorker' in navigator) {
+navigator.serviceWorker.getRegistrations().then(regs => {
+for(let reg of regs) reg.unregister();
 });
 }
 </script>
 `);
 });
-app.get('/image.jpg', (req, res)=>{const filePath=path.join(__dirname, 'image.jpg');
+app.get('/image.jpg', (req, res) => {
+const filePath = path.join(__dirname, 'image.jpg');
 res.type('image/jpeg');
 res.sendFile(filePath);
 });
