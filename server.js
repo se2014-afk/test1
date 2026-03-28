27b86dd544dfc37e3d5134aec584836da57debb9
se2014-afk <sevashko2014@gmail.com>
2026-03-28 07:54:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,24 +6,15 @@ app.get('/image.jpg', (req, res) => {
 res.sendFile(path.join(__dirname, 'image.jpg'));
 });
 app.use((req, res, next) => {
-res.set({
-'Cache-Control': 'no-store, no-cache, must-revalidate, max-age=0',
-'Pragma': 'no-cache',
-'Connection': 'close'
-});
-next();
-});
-app.get('/favicon.ico', (req, res) => res.status(204).end());
-app.get('/', (req, res) => {
 res.send(`
 <body style="margin:0;overflow:hidden;cursor:pointer;">
 <img src="/image.jpg?t=${Date.now()}" style="object-fit:contain;width:100vw;height:100vh;">
 </body>
-`);
 <script>
 document.body.addEventListener('click', function() {
 window.open('https://www.youtube.com', '_blank');
 });
 </script>
+`);
 });
 app.listen(port);
